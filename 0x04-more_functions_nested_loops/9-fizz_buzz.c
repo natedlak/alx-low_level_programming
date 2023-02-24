bd1a2df88b422dc26f6a851fdd226c2f348991aa
natedlak <edenteklewold08@gmail.com>
2023-02-24 12:46:27 -0800

    Debugging

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,15 +15,15 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i == 100)
-			printf("%s", b);
+			printf("%s ", b);
 		else if ((i % 3 == 0) && (i % 5 == 0))
-			printf("%s", ab);
+			printf("%s ", ab);
 		else if (i % 3 == 0)
-			printf("%s", a);
+			printf("%s ", a);
 		else if (i % 5 == 0)
-			printf("%s", b);
+			printf("%s ", b);
 		else
-			printf("%d", i);
+			printf("%d ", i);
 	}
 	printf("\n");
 	return (0);
