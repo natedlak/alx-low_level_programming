70253e78bec3fb7adc4681c17d28a43d7eff7af4
natedlak <edenteklewold08@gmail.com>
2023-03-03 09:30:23 -0800

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
 
-/**
- * main - prints a[2] = 98, followed by a new line
- * Return:0
- */
 int main(void)
 {
 	int n;
@@ -12,13 +8,13 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-	/**
-	 * write your line of code here
+	/*
+	 * write your line of code here...
 	 * Remember:
 	 * - you are not allowed to use a
 	 * - you are not allowed to modify p
 	 * - only one statement
-	 * - you're not allowed to code anything else than this line of code
+	 * - you are not allowed to code anything else than this line of code
 	 */
 	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
